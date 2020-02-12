2312de222745ecbc927905b379d4397a635f351e
santiagofp9 <santiagofp9@gmail.com>
2020-02-12 16:58:58 +0100

    Crud admin
    Cuenta de admin creada
    user: santiagofp9
    psswrd: santiagofp9
    
    El crud funciona por detrás, no hay tableros ni nada creado.

--- Trello/appBD/admin.py
+++ Trello/appBD/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+class TableroAdmin(admin.ModelAdmin):
+    list_display=('nombreTab',)
+
+class ListaAdmin(admin.ModelAdmin):
+    list_display=('nombreList','fkTablero')
+
+class TarjetaAdmin(admin.ModelAdmin):
+    list_display=('nombreTarj','descripcion','fkLista')
+
+admin.site.register(Tablero,TableroAdmin)
+admin.site.register(Lista,ListaAdmin)
+admin.site.register(Tarjeta,TarjetaAdmin)
