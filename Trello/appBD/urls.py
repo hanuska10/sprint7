5fdbfcd35764d9370560b65e6fc9147505acb200
santiagofp9 <santiagofp9@gmail.com>
2020-02-18 15:48:21 +0100

    solo un url para consultar

--- Trello/appBD/urls.py
+++ Trello/appBD/urls.py
@@ -4,10 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('crearTar/',views.crearTarjeta, name='crearTarjeta'),
-    path('consulTar/',views.consultarTarjeta, name='consultarTarjeta'),
     path('ediTar/',views.editarTarjeta, name='editarTarjeta'),
     path('crearLis/',views.crearLista, name='crearLista'),
-    path('consuLis/',views.consultarLista, name='consultarLista'),
     path('ediLis/',views.editarLista, name='editarLista'),
     path('crearTab/',views.crearTablero, name='crearTablero'),
     path('consulTab/',views.consultarTablero, name='consultarTablero'),
